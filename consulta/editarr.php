31bcbe0fe182bd299b7c6fa017afcae1a62c1035
bmatias74 <brunotchiyevo74@gmail.com>
2024-04-22 03:18:07 +0100

    falta alguns links

--- consulta/editarr.php
+++ consulta/editarr.php
@@ -14,6 +14,8 @@ $linha = mysqli_fetch_assoc($resultado);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Editar restaurante</title>
     <link rel="stylesheet" href="./CSS/Login1.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
 </head>
 <body>
 <header>
