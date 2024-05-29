64e038f42f01e0744baa7d9fb7e7dd8236d2dd8a
bmatias74 <brunotchiyevo74@gmail.com>
2024-05-29 15:53:58 +0100

    Update slide.js

--- slide.js
+++ slide.js
@@ -4,32 +4,34 @@ let contador = 1;
 const slidesContainer = document.querySelector('.slides');
 const radioButtons = document.querySelectorAll('#radio1, #radio2, #radio3, #radio4');
 const navegador = document.querySelectorAll('#radion1, #radion2, #radion3, #radion4');
-function updateSlider(index){
-    navegador.forEach((radion, ind) => {
-        radion.style.backgroundColor = '';
-        if (ind === index) {
-            radion.style.backgroundColor = 'yellow';
-        }
-    });
-}
 
-const marginLeft = -200 * index;
-slidesContainer.style.marginLeft = `${marginLeft}%`;
 radioButtons.forEach((radio, index) => {
     radio.addEventListener('change', () => {
         if (radio.checked) {
+            navegador.forEach((radion, ind) => {
+                 radion.style.backgroundColor = '';
+                 if (ind === index) {
+                     radion.style.backgroundColor = 'yellow';
+                 }
+             });
             
+             const marginLeft = -200 * index;
+            slidesContainer.style.marginLeft = `${marginLeft}%`;
         }
     });
 });
+
 setInterval(function() {
+
     document.getElementById('radio' + contador).checked = true;
     radioButtons[contador - 1].dispatchEvent(new Event ('change'));
     contador++;
     if (contador > 4) {
         contador = 1;
     }
-}, 10000);
+   
+    
+}, 1000);
 
   });
 
